39a761dc29cda747556432b5cb9dc50bb7d95668
Divya Manchala <mahesh288646@gmail.com>
2021-05-07 23:16:51 -0400

    second

--- AWS/Prep6/main.tf
+++ AWS/Prep6/main.tf
@@ -3,6 +3,14 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "labs3april42021"
+    key    = "/app/terraform-state"
+    region = "us-east-1"
+  }
+}
+
 module "vpc" {
   source           = "./modules/services/vpc"
   cidr_blocknumber = "10.0.0.0/16"
